447c615a486db8b0fa49bf4eb1afd93fd03b86f8
David Jencks <djencks@apache.org>
2020-01-17 23:19:54 -0800

    copy some infrastructure from antora

---
+++ gulp.d/tasks/test.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const { buildArtifactUrl } = require('../lib/gitlab')
+const run = require('../lib/run-command')
+
+module.exports = (files, analyzeCodeCoverage = false) => () => {
+  const args = [...files]
+  if (process.env.CI) args.unshift('--forbid-only', '--timeout', '5000')
+  if (analyzeCodeCoverage) {
+    let onSuccess
+    if (process.env.GITLAB_CI) {
+      const coverageReportUrl = buildArtifactUrl(
+        process.env.CI_PROJECT_PATH,
+        process.env.CI_JOB_ID,
+        'coverage/lcov-report/index.html'
+      )
+      onSuccess = () => console.log('Coverage report: ' + coverageReportUrl)
+    }
+    return run('nyc', ['_mocha'].concat(args), onSuccess)
+  } else {
+    return run('_mocha', args)
+  }
+}
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const opts = require('yargs-parser')(process.argv.slice(2))
+
+const { series } = require('gulp')
+const createTask = require('./gulp.d/lib/create-task')
+const exportTasks = require('./gulp.d/lib/export-tasks')
+
+const { format, lint, test } = require('./gulp.d/tasks')
+const glob = opts.package
+  ? {
+    sourceFiles: [`packages/${opts.package}/{lib,test}/**/*.js`],
+    testFiles: [`packages/${opts.package}/test/**/*-test.js`],
+  }
+  : {
+    sourceFiles: ['gulpfile.js', '{gulp.d,lib-example,scripts,test}/**/*.js', 'packages/*/{lib,test}/**/*.js'],
+    testFiles: ['test/**/*-test.js', 'packages/*/test/**/*-test.js'],
+  }
+const sharedOpts = { '--package <name>': 'Only run on files in the specified package' }
+
+const lintTask = createTask({
+  name: 'lint',
+  desc: 'Lint JavaScript files using eslint (JavaScript Standard profile)',
+  opts: sharedOpts,
+  call: lint(glob.sourceFiles),
+})
+
+const formatTask = createTask({
+  name: 'format',
+  desc: 'Format JavaScript files using prettier (JavaScript Standard profile)',
+  opts: sharedOpts,
+  // convert globs to absolute path to workaround https://github.com/prettier/prettier-eslint-cli/issues/208
+  call: format(glob.sourceFiles.map((it) => `${__dirname}/${it}`)),
+})
+
+const testTask = createTask({
+  name: 'test',
+  desc: 'Run the test suite',
+  opts: { ...sharedOpts, '--watch': 'Watch files and run the test suite whenever a file is changed' },
+  call: test(glob.testFiles, process.env.COVERAGE === 'true' || process.env.CI),
+  loop: opts.watch ? glob.sourceFiles : false,
+})
+
+const buildTask = createTask({
+  name: 'build',
+  desc: 'Run the test suite followed by the linter',
+  opts: sharedOpts,
+  call: series(testTask, lintTask),
+})
+
+module.exports = exportTasks(buildTask, buildTask, lintTask, formatTask, testTask)
